674c4e99282bed682fecb3dbe1c1701fbb1a3b09
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-03 01:54:34 +0800

    hw4 init commit

---
+++ p1/configs/nerf/hotdog.py
@@ -0,0 +1,10 @@
+_base_ = "../default.py"
+
+expname = "dvgo_hotdog"
+basedir = "./logs/nerf_synthetic"
+
+data = dict(
+    datadir="/content/hw4_data/hw4_data/hotdog",
+    dataset_type="blender",
+    white_bkgd=True,
+)
