7f5bbfe26d98b62a4b942afb3dea5aa9db4a0596
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-04 02:38:54 +0800

    hw4 p2 init

---
+++ p2/run.py
@@ -0,0 +1,29 @@
+import torch
+from byol_pytorch import BYOL
+from torchvision import models
+
+resnet = models.resnet50(pretrained=True)
+
+learner = BYOL(
+    resnet,
+    image_size=128,
+    hidden_layer="avgpool",
+    use_momentum=False,  # turn off momentum in the target encoder
+)
+
+opt = torch.optim.Adam(learner.parameters(), lr=3e-4)
+
+
+def sample_unlabelled_images():
+    return torch.randn(20, 3, 256, 256)
+
+
+for _ in range(100):
+    images = sample_unlabelled_images()
+    loss = learner(images)
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+
+# save your improved network
+torch.save(resnet.state_dict(), "./ckpt/improved-net.pt")
