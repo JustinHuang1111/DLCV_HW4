1416c3b0a3d3ec453133f3f0dd311c6d8d668cc2
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-04 11:42:26 +0800

    hw4 p2 fix bugs

--- p2/run.py
+++ p2/run.py
@@ -1,18 +1,29 @@
+import kornia
 import torch
+import torch.nn as nn
 from byol_pytorch import BYOL
 from torchvision import models
 from tqdm.auto import tqdm
-
+import os
 resnet = models.resnet50(pretrained=False)
+os.makedirs("./ckpt")
+augment_fn = nn.Sequential(kornia.augmentation.RandomHorizontalFlip())
+
+augment_fn2 = nn.Sequential(
+    kornia.augmentation.RandomHorizontalFlip(),
+    kornia.filters.GaussianBlur2d((3, 3), (1.5, 1.5)),
+)
 
 learner = BYOL(
     resnet,
     image_size=128,
     hidden_layer="avgpool",
     use_momentum=False,  # turn off momentum in the target encoder
+    augment_fn= = augment_fn,
+    augment_fn2=augment_fn2
 )
 
-opt = torch.optim.Adam(learner.parameters(), lr=3e-4)
+opt = torch.optim.Adam(learner.parameters(), lr=3e-4)   
 
 
 def sample_unlabelled_images():
