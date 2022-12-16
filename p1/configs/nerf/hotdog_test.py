1bbed1b2cc13fd238e910d03e11cd2bc6edd15ce
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-17 04:02:47 +0800

    hw4 fix

--- p1/configs/nerf/hotdog_test.py
+++ p1/configs/nerf/hotdog_test.py
@@ -1,10 +1,10 @@
 _base_ = "../default.py"
 
 expname = "dvgo_hotdog"
-basedir = "./logs/nerf_synthetic"
+basedir = "./p1/logs/nerf_synthetic"
 
 data = dict(
-    datadir="/content/hw4_data/hw4_data/hotdog",
+    # datadir="/content/hw4_data/hw4_data/hotdog",
     dataset_type="blender_test",
     white_bkgd=True,
 )
