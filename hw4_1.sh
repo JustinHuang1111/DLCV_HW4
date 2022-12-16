96fcb8a36e4805636147d3a87d470b9c176f239a
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-17 03:47:45 +0800

    hw4 fix

--- hw4_1.sh
+++ hw4_1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python run.py --config configs/nerf/hotdog_test.py --render_test --render_only --dump_images --json_dir $1 --outpath $2
+python ./p1/run.py --config configs/nerf/hotdog_test.py --render_test --render_only --dump_images --json_dir $1 --outpath $2
 # TODO - run your inference Python3 code
