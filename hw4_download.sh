c8d19b8deffd8777f5c6be61e7f8afabd34a1a84
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-12-17 03:37:18 +0800

    hw4 download.sh

--- hw4_download.sh
+++ hw4_download.sh
@@ -1,2 +1,4 @@
-wget https://www.dropbox.com/s/mcofkcf53brg9jc/300_fourth_finetune.pt?dl=1 -O ./ckpt/300_fourth_finetune.pt
-wget https://www.dropbox.com/s/29hgwf4amc88y43/400minesgd_first_finetune.pt?dl=1 -O ./ckpt/400minesgd_first_finetune.pt
+wget https://www.dropbox.com/s/mcofkcf53brg9jc/300_fourth_finetune.pt?dl=1 -O ./p2/ckpt/300_fourth_finetune.pt
+wget https://www.dropbox.com/s/29hgwf4amc88y43/400minesgd_first_finetune.pt?dl=1 -O ./p2/ckpt/400minesgd_first_finetune.pt
+wget https://www.dropbox.com/s/e29sct5dpaa4akr/fine_last.tar?dl=1 -O ./p1/logs/nerf_synthetic/dvgo_hotdog/fine_last.tar
+wget https://www.dropbox.com/s/02khef9u8oxdtlr/coarse_last.tar?dl=1 -O ./p1/logs/nerf_synthetic/dvgo_hotdog/coarse_last.tar
